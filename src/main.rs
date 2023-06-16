415f88558c111388e81696d102ecebbe9b984d22
mov.ax.38 <mov.ax.38@gmail.com>
2023-06-16 21:01:23 +0300

    Basic rocket server

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use rocket::{launch, put, routes};
+
+#[put("/run", data = "<functions>")]
+fn run(functions: String) -> String {
+    match nast::run(functions.as_str()) {
+        Ok(v) => format!("{v:?}"),
+        Err(e) => format!("Error: {e}"),
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![run])
+}
